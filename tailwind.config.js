af233bb19428661fa590cddfb857a97dc208d534
Thomas <bdachai308@gmail.com>
2024-09-13 10:47:12 +0700

    Add dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.{html,js}",
+    "./src/**/*.{html,js}",
+    "./assets/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
